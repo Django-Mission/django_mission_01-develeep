c8c443115c9cbef32ff53b9b6401a0d8b64b7349
shlee <83402978+develeep@users.noreply.github.com>
2022-03-30 19:04:43 +0900

    Update views.py

--- challenge/demos/views.py
+++ challenge/demos/views.py
@@ -15,12 +15,12 @@ def lotto_result(request):
     lotto_number = list()
     # 게임 수 받기
     game = int(request.GET.get('game', 1))
-    # 1~46 숫자 배열
-    pull_number = list(index for index in range(1, 46))
+    # 1~45 숫자 배열
+    pull_number = list(index for index in range(1, 45))
 
     # 게임 수만큼 로또 번호 추출
     for _ in range(game):
-        # random.sample을 통해 1~46의 숫자중 6개를 추출
+        # random.sample을 통해 1~45의 숫자중 6개를 추출
         lotto_number.append(random.sample(pull_number, 6))
 
     return render(request, 'lotto_result.html', {'lotto_number': lotto_number, 'game': game})
